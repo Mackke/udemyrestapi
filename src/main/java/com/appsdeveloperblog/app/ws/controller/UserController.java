cdb66f5593c59acbd343078a1e09205c02fea715
mackke <mackke90@gmail.com>
2018-10-16 00:06:23 +0200

    second commit of my project

---
+++ src/main/java/com/appsdeveloperblog/app/ws/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.appsdeveloperblog.app.ws.controller;
+
+
+import com.appsdeveloperblog.app.ws.service.UserService;
+import com.appsdeveloperblog.app.ws.dto.UserDto;
+import com.appsdeveloperblog.app.ws.request.UserDetailRequestModel;
+import com.appsdeveloperblog.app.ws.ui.model.request.response.UserRest;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("users") //http://localhost:8080/users
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public String getUser(){
+        return "get user was called";
+    }
+
+    @PostMapping
+    public UserRest createUser(@RequestBody UserDetailRequestModel userDetails){
+        UserRest returnValue = new UserRest();
+
+        UserDto userDto = new UserDto();
+        BeanUtils.copyProperties(userDetails, userDto);
+
+        UserDto createdUser = userService.createUser(userDto);
+        BeanUtils.copyProperties(createdUser, returnValue);
+
+        return returnValue;
+    }
+
+    @PutMapping
+    public String updateUser(){
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser(){
+        return "delete user was called";
+    }
+
+}
---
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -0,0 +1,52 @@
+package com.appsdeveloperblog.app.ws.service.impl;
+
+import com.appsdeveloperblog.app.ws.UserRepository;
+import com.appsdeveloperblog.app.ws.dto.Utils;
+import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
+import com.appsdeveloperblog.app.ws.service.UserService;
+import com.appsdeveloperblog.app.ws.dto.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    Utils utils;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public UserDto createUser(UserDto user) {
+
+        if(userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("Record already exists");
+
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+
+        String publicUserId = utils.generateUserId(30);
+        userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userEntity.setUserId(publicUserId);
+
+        UserEntity storedUserDetails = userRepository.save(userEntity);
+
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(storedUserDetails, returnValue);
+
+
+        return returnValue;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
+}
